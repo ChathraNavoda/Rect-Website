16fec71933b6e0b199efdf0f63abaa0f81b78cd4
Chathra Navoda <chathra96@gmail.com>
2023-01-22 13:27:34 +0530

    function to open/close menu

--- space/src/components/pages/Header.js
+++ space/src/components/pages/Header.js
@@ -1,23 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Close, MenuOutlined } from "@material-ui/icons";
 import { Link } from "react-router-dom";
 import moonLogo from "../assets/moon-1.png";
 import "../styles/Header.css";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+
+  const showMenu = () => {
+    setOpen(!open);
+  };
   return (
     <div className="header">
       <div className="header-logo">
         <img src={moonLogo} alt="moon-logo" />
       </div>
       <div className="hamburger-icon">
-        <MenuOutlined />
+        <MenuOutlined className="icon" onClick={showMenu} />
       </div>
 
       <nav>
-        <ul className="list-items">
+        <ul className={open ? "list-items active" : "list-items"}>
           <div className="close-icon">
-            <Close className="icon" />
+            <Close className="icon" onClick={showMenu} />
           </div>
           <li>
             <Link to="/">Home</Link>
--- space/src/components/pages/Section.js
+++ space/src/components/pages/Section.js
@@ -1,4 +1,5 @@
 import React from "react";
+import '../styles/Section.css'
 
 const Section = () => {
   return <div className="section"></div>;
