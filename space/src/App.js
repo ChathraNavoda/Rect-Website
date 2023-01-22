3187c2406e5a363cf24d9e8f8e919967648a10b4
Chathra Navoda <chathra96@gmail.com>
2023-01-22 14:34:20 +0530

    add sections

--- space/src/App.js
+++ space/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/pages/Header";
+import Section from "./components/pages/Section";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Section />
     </div>
   );
 }
--- space/src/components/pages/Section.js
+++ space/src/components/pages/Section.js
@@ -1,8 +1,16 @@
 import React from "react";
-import '../styles/Section.css'
+import "../styles/Section.css";
 
 const Section = () => {
-  return <div className="section"></div>;
+  return (
+    <div className="section">
+      <div className="change-box">
+        <button>Planets</button>
+        <button>Nature</button>
+        <button>Space</button>
+      </div>
+    </div>
+  );
 };
 
 export default Section;
