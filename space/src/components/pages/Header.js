a33736d1ecd7805d5d00ce5d787e72ebe5596199
Chathra Navoda <chathra96@gmail.com>
2023-01-22 17:05:48 +0530

    add links to header

--- space/src/components/pages/Header.js
+++ space/src/components/pages/Header.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
 import { Close, MenuOutlined } from "@material-ui/icons";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import moonLogo from "../assets/moon-1.png";
 import "../styles/Header.css";
@@ -10,11 +10,13 @@ const Header = () => {
   const showMenu = () => {
     setOpen(!open);
   };
+
   return (
     <div className="header">
       <div className="header-logo">
         <img src={moonLogo} alt="moon-logo" />
       </div>
+
       <div className="hamburger-icon">
         <MenuOutlined className="icon" onClick={showMenu} />
       </div>
@@ -24,12 +26,23 @@ const Header = () => {
           <div className="close-icon">
             <Close className="icon" onClick={showMenu} />
           </div>
+
           <li>
             <Link to="/">Home</Link>
+          </li>
+          <li>
             <Link to="/">Destinations</Link>
+          </li>
+          <li>
             <Link to="/">Designs</Link>
+          </li>
+          <li>
             <Link to="/">Pricing</Link>
-            <Link to="/">Contact</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
             <Link to="/">About</Link>
           </li>
         </ul>
