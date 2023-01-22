68c51d1036f341b07db453a796e94b5594e2959b
Chathra Navoda <chathra96@gmail.com>
2023-01-22 12:58:07 +0530

    nav bar list items

--- space/src/components/pages/Header.js
+++ space/src/components/pages/Header.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Close, MenuOutlined } from "@material-ui/icons";
+import { Link } from "react-router-dom";
 import moonLogo from "../assets/moon-1.png";
 
 const Header = () => {
@@ -7,6 +9,25 @@ const Header = () => {
       <div className="header-logo">
         <img src={moonLogo} alt="moon-logo" />
       </div>
+      <div className="hamburger-icon">
+        <MenuOutlined />
+      </div>
+
+      <nav>
+        <ul className="list-items">
+          <div className="close-icon">
+            <Close className="icon" />
+          </div>
+          <li>
+            <Link to="/">Home</Link>
+            <Link to="/">Destinations</Link>
+            <Link to="/">Designs</Link>
+            <Link to="/">Pricing</Link>
+            <Link to="/">Contact</Link>
+            <Link to="/">About</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
